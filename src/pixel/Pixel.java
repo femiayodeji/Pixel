7be0529cb06d791503db44242dc5ddf30f3f4b94
Instincts <feminadio@gmail.com>
2018-03-10 20:50:18 +0100

    first commit

---
+++ src/pixel/Pixel.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pixel;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author feminstincts
+ */
+public class Pixel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String title = "Pixel";        
+        JFrame home = new Home();        
+        home.setVisible(true);
+    }
+    
+}
